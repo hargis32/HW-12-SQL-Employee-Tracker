3cfd95ce317fe6cf6c93fb6d6f0367efd1416c21
Austin Pratt <hargis32@gmail.com>
2022-01-31 00:38:22 -0600

    server.js and schema now functional

--- db/schema.sql
+++ db/schema.sql
@@ -6,14 +6,14 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    department_name VARCHAR(30) NOT NULL,
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE emp_role (
     id INT AUTO_INCREMENT NOT NULL PRIMARY  KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
-    department_id INT,
+    department_id INT
 );
 
 CREATE TABLE employee (
@@ -21,5 +21,5 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL, 
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT,
+    manager_id INT
 );
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         user: 'root',
-        password: '',
+        password: 'Hargis23!',
         database: 'employee_db'
     },
     console.log('connected to the database!')
@@ -18,7 +18,8 @@ db.connect(function (err) {
 })
 
 const startInquire = () => {
-    inquirer.prompt([
+    inquirer
+    .prompt([
         {
             type: "list",
             message: "What do you want to do?",
@@ -35,8 +36,8 @@ const startInquire = () => {
             ]
         }
     ])
-    .then(function (todo) {
-        switch (todo.choice) {
+    .then(function (answer) {
+        switch (answer.todo) {
             case "View all departments":
                 viewDepartments();
                 break;
@@ -88,3 +89,91 @@ const viewEmployees = () => {
         startInquire();
     })
 };
+
+const addDepartment = () => {
+    inquirer
+        .prompt([
+            {
+                name: "department_name",
+                type: "input",
+                message: "What department would you like to add?"
+            }
+        ])
+        .then(function (answer) {
+            db.query('INSERT INTO department SET ?',
+            {
+                department_name: answer.department_name
+            },
+            function (err, res) {
+                if (err) throw err;
+                startInquire();
+            });
+        });
+};
+
+const addRole = () => {
+    inquirer
+        .prompt([
+            {
+                name: "title",
+                type: "input",
+                message: "What role would you like to add?"
+            },
+            {
+                name: "salary",
+                type: "input",
+                message: "What is the salary for this new role?"
+            },
+            {
+                name: "department_id",
+                type: "input",
+                message: "What department does this role belong to?"
+            }
+        ])
+        .then(function (answer) {
+            db.query('INSERT INTO emp_role SET ?', 
+            {
+                title: answer.title,
+                salary: answer.salary,
+                department_id: answer.department_id
+            },
+            function (err, res) {
+                if (err) throw err;
+                startInquire();
+            });
+        });
+};
+
+const addEmployee = () => {
+    inquirer
+        .prompt([
+                {
+                    name: "first_name",
+                    type: "input",
+                    message: "What is the employee's first name?"
+                },
+                {
+                    name: "last_name",
+                    type: "input",
+                    message: "What is the employee's last name?"
+                },
+                {
+                    name: "role_id",
+                    type: "input",
+                    message: "What is the employee's role id?"
+                }
+            ])
+            .then(function(answer) {
+                db.query("INSERT INTO employee SET ?", 
+                {
+                    first_name: answer.first_name,
+                    last_name: answer.last_name,
+                    role_id: answer.role_id
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    startInquire();
+                });
+            });
+    };
+
